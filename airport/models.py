fc7991869895ca2ffe7f8633a8ec87ec63e92b4b
Empty <make772@ukr.net>
2025-04-29 16:51:06 +0300

    add Airport model

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+    closest_big_city = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
