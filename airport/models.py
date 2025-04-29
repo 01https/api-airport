f271376268bbed8cbd7d33edc2d14ca26d451389
Empty <make772@ukr.net>
2025-04-29 16:52:03 +0300

    add AirplaneType model

--- airport/models.py
+++ airport/models.py
@@ -17,3 +17,10 @@ class Airplane(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+
+    def __str__(self):
+        return self.name
